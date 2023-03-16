02a633f0f70426d59096abb34b525f67b88794aa
asamisellah <asami.rachael@gmail.com>
2023-03-17 00:41:02 +0300

    onboard drivers

--- controllers/passangerController.ts
+++ controllers/passangerController.ts
@@ -1,12 +1,41 @@
 import { Request, Response } from "express";
-import { passangers } from "../dto/data";
+import { passangers } from "../models/data";
+import { encryptPass } from "../services/commonUtils";
+import { User, ResponseType, Status } from "../types";
 
-export const createUser = (req: Request, res: Response) => {
-  let newUser = req.body;
 
-  passangers.push({ id: passangers.length, ...newUser });
-  // console.log(passangers)
-  res.status(200).send(passangers);
+export const createUser = (req: Request, res: Response<ResponseType>) => {
+  const reqbody: User = req.body;
+    // Hash Password
+    reqbody.password = await encryptPass(reqbody.password);
+
+    // save driver in db
+    const newDriver = new DriverModel(reqbody);
+    await newDriver
+      .save()
+      .then((doc) => {
+        res.status(201).json({
+          status: Status.SUCCESS,
+          message: "Driver created successfully",
+          data: doc,
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+        res.status(400).json({
+          status: Status.FAILURE,
+          message: "Failed to create driver",
+          errors: err.message,
+        });
+      });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({
+      status: Status.FAILURE,
+      message: "Internal server error",
+      errors: err,
+    });
+  }
 };
 
 export const getUser = (req: Request, res: Response) => {
---
+++ services/commonUtils.ts
@@ -0,0 +1,6 @@
+import bcrypt from "bcrypt";
+
+export const encryptPass = async (pass: String | any) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(pass, salt);
+};
